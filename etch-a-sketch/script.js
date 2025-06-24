ca2479c89c393e5b155ce9bddbc331ce66f77d3b
kubakalisciak <168641088+kubakalisciak@users.noreply.github.com>
2025-06-24 14:56:09 +0200

    Implement dynamic grid sizes and generating them

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,22 +1,34 @@
 const gridContainer = document.getElementById('grid-container');
+const generateBtn = document.getElementById('generateBtn');
+const cellInput = document.getElementById('cellInput');
+let sideOfSquare = 16;
 
-sideOfSquare = 16;
-for (let i=0; i < sideOfSquare*sideOfSquare; i++) {
-    cell = document.createElement('div');
-    cell.id = "cell" + i;
-    cell.classList.add('grid-cell')
-    gridContainer.appendChild(cell)
+function generateGrid(size) {
+    document.documentElement.style.setProperty("--cell-width", 400 / size + 'px');
+    for (let i=0; i < size*size; i++) {
+        cell = document.createElement('div');
+        cell.id = "cell" + i;
+        cell.classList.add('grid-cell')
+        gridContainer.appendChild(cell)
+    }
+    for (let i=0; i < size*size; i++) {
+        let cell = document.getElementById('cell'+i);
+        cell.addEventListener('mouseover', () => {
+            if (cell.classList.contains('black')) {
+                // arr = cell.classList.splice(cell.classList.indexOf('black'));
+                // cell.classList = arr;
+                cell.classList.remove('black');
+            }
+            else {
+                cell.classList.add('black');
+            }
+        });
+    }
 }
-for (let i=0; i < sideOfSquare*sideOfSquare; i++) {
-    let cell = document.getElementById('cell'+i);
-    cell.addEventListener('mouseover', () => {
-        if (cell.classList.contains('black')) {
-            // arr = cell.classList.splice(cell.classList.indexOf('black'));
-            // cell.classList = arr;
-            cell.classList.remove('black');
-        }
-        else {
-            cell.classList.add('black');
-        }
-    });
-}
+
+generateBtn.addEventListener('click', () => {
+    gridContainer.replaceChildren(); // kill all children
+    generateGrid(parseInt(cellInput.value));
+});
+
+generateGrid(16);
