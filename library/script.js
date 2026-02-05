40e0c0456eefa120ef7cdeb5cb535deae94175bc
kubakalisciak <168641088+kubakalisciak@users.noreply.github.com>
2026-02-05 19:53:15 +0100

    Refactor the book card creation logic and add styling

--- library/script.js
+++ library/script.js
@@ -1,20 +1,20 @@
 const myLibrary = []
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.isRead = isRead;
     this.id = crypto.randomUUID()
 
     this.info = function() {
-        return `${title} by ${author}, ${pages}, ${read}\n
+        return `${title} by ${author}, ${pages}, ${isRead}\n
         UUID: ${id}`
     }
 }
 
-function addBook(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read))
+function addBook(title, author, pages, isRead) {
+    myLibrary.push(new Book(title, author, pages, isRead))
 }
 
 function displayBooksOnPage() {
@@ -24,24 +24,26 @@ function displayBooksOnPage() {
         const bookCard = document.createElement('div')
         bookCard.classList.add('card')
 
-        const titleLabel = document.createElement('p')
-        titleLabel.textContent = "Title: " + book.title
-        bookCard.appendChild(titleLabel)
-        const authorLabel = document.createElement('p')
-        authorLabel.textContent = "Author: " + book.author
-        bookCard.appendChild(authorLabel)
-        const pagesLabel = document.createElement('p')
-        pagesLabel.textContent = "Pages: " + book.pages
-        bookCard.appendChild(pagesLabel)
-        const readLabel = document.createElement('p')
-        readLabel.textContent = "Read: " + book.read
-        bookCard.appendChild(readLabel)
+        bookCard.appendChild(generateBookLabel(book, 'title', 'h2', ''))
+        bookCard.appendChild(generateBookLabel(book, 'author', 'p', 'Author: '))
+        bookCard.appendChild(generateBookLabel(book, 'pages', 'p', 'Pages: '))
+        bookCard.appendChild(generateBookLabel(book, 'isRead', 'p', 'Is read? '))
 
         container.appendChild(bookCard)
     }
 }
 
-addBook('title', 'auth', '111', 'nope')
-addBook('title2', 'auth', '211', 'nope')
+function generateBookLabel(object, property, type, text) {
+    const label = document.createElement(type)
+    label.textContent = text + object[property]
+    return label
+}
+
+addBook('title', 'author', 'pages', true)
+addBook('title', 'author', 'pages', true)
+addBook('title', 'author', 'pages', true)
+addBook('title', 'author', 'pages', true)
+addBook('title', 'author', 'pages', true)
+
 
 displayBooksOnPage()
